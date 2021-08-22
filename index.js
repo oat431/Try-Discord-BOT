2b1c83433dc89d999c8c37fba728cc30dccbf42f
Oralita <oat431@gmail.com>
2021-08-22 15:46:25 +0700

    [🥢 add] : event dir

--- index.js
+++ index.js
@@ -1,36 +1,18 @@
 const fs = require('fs');
-const authToken = require('./util/config.json');
-const { Client, Collection, Intents } = require('discord.js');
+const { token } = require('./util/config.json');
+const { Client, Intents } = require('discord.js');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-client.commands = new Collection();
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-const commandFile = fs.readdirSync('./Command').filter(file => file.endsWith('.js'));
+const eventFile = fs.readdirSync('./Event').filter(file => file.endsWith('.js'));
 
-for(const file of commandFile) {
-    const command = require(`./Command/${file}`);
-    client.commands.set(command.data.name,command)
+for(const file of eventFile) {
+    const event = require(`./Event/${file}`);
+    if(event.once){
+        client.once(event.name, (...args) => event.execute(... args, client));
+    }else{
+        client.on(event.name, (...args) => event.execute(... args, client));
+    }
 }
 
-client.on('ready', ()=> {
-    console.log(`Logged in as ${client.user.tag}!`);
-})
-
-client.on('interactionCreate', async interaction => {
-	console.log(interaction);
-    if(!interaction.isCommand()) return;
-
-    const command = client.commands.get(interaction.commandName)
-
-    if(!command) return
-
-    try{
-        await command.execute(interaction);
-    }catch(error){
-        console.error(error);
-        await interaction.reply({content:"some error happen",ephemeral:true})
-    }
-    
-});
-
-client.login(authToken.token);
+client.login(token);
