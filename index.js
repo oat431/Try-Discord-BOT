45aaf1c77e7098c769129717d3b9f1b706dd39a4
Oralita <oat431@gmail.com>
2021-08-23 21:39:57 +0700

    [🍇 refractor] : the event

--- index.js
+++ index.js
@@ -1,36 +1,25 @@
 const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./util/config.json');
-const { bot } = require('./deploy-command');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
 client.commands = new Collection();
+
 const commandFiles = fs.readdirSync('./Command').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync('./Event').filter(file => file.endsWith('.js'));
 
 for(const file of commandFiles){
 	const command = require(`./Command/${file}`);
 	client.commands.set(command.data.name,command);
 }
 
-client.once('ready', () => {
-    bot();
-	console.log('Ready!');
-});
-
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-
-	const command = client.commands.get(interaction.commandName);	
-
-	if(!command) return;
-
-	try{
-		await command.execute(interaction);
-	}catch(error){
-		console.error(error);
-		return interaction.reply({content:"This command face some problem while executing, retry it maybe?"})
+for(const file of eventFiles){
+	const event = require(`./Event/${file}`);
+	if(event.once){
+		client.once(event.name,(...args) => event.execute(...args));
+	}else{
+		client.on(event.name,(...args) => event.execute(...args,client));
 	}
-});
+}
 
 client.login(token);
