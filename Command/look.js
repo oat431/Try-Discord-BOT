a51579a0021695fa7fcc3b511708a139c4ea7583
Oralita <oat431@gmail.com>
2021-08-24 17:37:04 +0700

    [🧁 add]: choice

--- Command/look.js
+++ Command/look.js
@@ -3,8 +3,23 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('how-am-i-look')
-        .setDescription('Bot will admire you'),
+        .setDescription('Bot will admire you')
+        .addStringOption(option => 
+            option.setName("select-your-sex")
+                .setDescription("Just select it dammit")
+                .setRequired(true)
+                .addChoice("male","male")
+                .addChoice("female","female") 
+            ),
     async execute(interaction) {
-        return interaction.reply('You look so Cute/Handsome.\nThank you for your existance.')
+        const data = interaction.options.getString("select-your-sex")
+        console.log(data)
+        let msg = "";
+        if(data === 'male'){
+            msg = "You look so Handsome.\nThank you for your existance."
+        }else if(data === 'female'){
+            msg = "You look so Cute.\nYour existance is worth to admire."
+        }
+        return interaction.reply(msg)
     }
 }
